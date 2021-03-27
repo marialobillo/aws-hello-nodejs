0c19b92607dda6fbf7fa3afcb6a2bd100ab3caef
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2021-03-27 18:55:42 +0100

    update port

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ const app = express();
 
 app.get('/', (req, res) => res.send("<h1>Expressjs with HTML"));
 
-app.listen(3000);
+app.listen(8080);
 
-console.log("Server on port 3000");
+console.log("Server on port 8080");
